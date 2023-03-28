a8b72bed5997804284e1687e376f8f83b63ab7f8
KrouriB <brice.krouri@gmail.com>
2023-03-28 16:05:33 +0200

    page d'acceuil

---
+++ view/home.php
@@ -0,0 +1,21 @@
+<div class="bloc connect">
+    <h1>Connectez Vous</h1>
+    
+    <form>
+        <div>
+            <label for="">Pseudo :</label>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <label for="">Mot de Passe :</label>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <input type="submit" value="Accpter">
+        </div>
+    </form>
+    <div>
+        <p>Vous n'etes pas encore inscrit ?</p>
+        <a href="#">Inscrivez bous ici</a>
+    </div>
+</div>
