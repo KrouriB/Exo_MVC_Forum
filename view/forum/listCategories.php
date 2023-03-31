295c5fc5990fc23110e11707dd3d9d8628e4287f
KrouriB <brice.krouri@gmail.com>
2023-03-31 16:30:05 +0200

    affichage en tableau

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -6,10 +6,20 @@ $categories = $result["data"]['categories'];
 
 <h1>liste categories</h1>
 
-<?php
-foreach($categories as $categorie ){
-
-    ?>
-    <p><a href="index.php?ctrl=forum&action=listTopicsForACategorie&id=<?= $categorie->getId() ?>"><?=$categorie->getNomCategorie()?></a></p>
+<table>
+    <thead>
+        <th>Catégorie</th>
+    </thead>
+    <tbody>      
     <?php
-}
+    foreach($categories as $categorie){
+
+        ?>
+        <tr>
+            <td><a href="index.php?ctrl=forum&action=listTopicsForACategorie&id=<?= $categorie->getId() ?>"><?=$categorie->getNomCategorie()?></a></td>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
