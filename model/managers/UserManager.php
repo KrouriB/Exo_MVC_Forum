e822ec6f3c6f443373a8f91572cba1f908c8732a
KrouriB <brice.krouri@gmail.com>
2023-03-31 09:37:08 +0200

    ajout fonction trouver un user pas son email

--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -14,5 +14,18 @@
             parent::connect();
         }
 
+        public function findOnebyEmail($email){
+            $sql = "
+                SELECT *
+                FROM ".$this->tableName." a
+                WHERE a.email = :email";
+
+            var_dump($sql);die;
+
+            return $this->getOneOrNullResult(
+                DAO::select($sql,['email'=>$email],false),
+                $this->className
+            );
+        }
 
     }
