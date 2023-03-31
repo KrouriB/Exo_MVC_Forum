ff577ed4c1e477cfa32532816a77238b99f97ab8
KrouriB <brice.krouri@gmail.com>
2023-03-31 10:15:36 +0200

    ajout du futur form

--- view/forum/listTopicsForACategorie.php
+++ view/forum/listTopicsForACategorie.php
@@ -13,3 +13,22 @@ foreach($topics as $topic){
     <p><a href="index.php?ctrl=forum&action=aTopic&id=<?= $topic->getId() ?>"><?=$topic->getNomTopic()?></a></p>
     <?php
 }
+?>
+
+<form action="#" methode="post">
+    <div>
+        <label for="">
+            Le nom de votre Sujet:
+        </label>
+        <input type="text" name="" id="">
+    </div>
+    <div>
+        <label for="">
+            Détail de votre sujet (un "1er message"):
+        </label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div>
+        <input type="submit" value="Ajouter votre sujet">
+    </div>
+</form>
