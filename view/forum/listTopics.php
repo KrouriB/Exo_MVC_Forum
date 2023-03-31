6715ad03c4c4ef6bfa6e0de53740717a4c795b25
KrouriB <brice.krouri@gmail.com>
2023-03-31 10:44:55 +0200

    ajout d'un futur formulaire a utiliser

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -13,3 +13,37 @@ foreach($topics as $topic){
     <p><a href="index.php?ctrl=forum&action=aTopic&id=<?= $topic->getId() ?>"><?=$topic->getNomTopic()?></a></p>
     <?php
 }
+?>
+
+<form action="#" methode="post">
+    <div>
+        <label for="">
+            Le nom de votre Sujet:
+        </label>
+        <input type="text" name="" id="">
+    </div>
+    <div>
+        <label for="">
+            Détail de votre sujet (un "1er message"):
+        </label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div>
+        <label for="">
+            Choissisez la catégorie :
+        </label>
+        <select name="" id="">
+            <option value="0">--Veuillez selcetionner une option--</option>
+            <?php /*
+            foreach(){
+                */?>
+                    <!--<option value="#">#</option>-->
+                    <?php/*
+            }
+            */ ?>
+        </select>
+    </div>
+    <div>
+        <input type="submit" value="Ajouter votre sujet">
+    </div>
+</form>
