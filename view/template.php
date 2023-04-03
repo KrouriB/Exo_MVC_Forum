fefb2f2f2eb281c311a0b557757c06a124000955
KrouriB <brice.krouri@gmail.com>
2023-04-03 10:46:11 +0200

    ajout lien font awesome

--- view/template.php
+++ view/template.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/50e50e8630.js" crossorigin="anonymous"></script>
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
@@ -141,3 +142,10 @@
     </footer>
 </body>
 </html>-->
+
+
+
+<!-- 
+<i class="fa-solid fa-lock"></i> verouiller
+<i class="fa-regular fa-lock-open"></i> déverouiller
+-->
