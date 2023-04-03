72226feae01a8dd1ee04d501b0bc8e23141a3e51
KrouriB <brice.krouri@gmail.com>
2023-04-03 17:05:45 +0200

    fonction creation utilisateur

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -32,5 +32,68 @@
             return ["view" => VIEW_DIR."security/register.php"];
         }
 
-        
+        public function newRegister(){
+            
+            $userManager = new UserManager();
+
+            if(isset($_POST['submit'])){
+                
+                $allUsers1 = $userManager->findAll(["pseudo", "ASC"]);
+                $allUsers2 = $userManager->findAll(["pseudo", "ASC"]);
+
+                // sanitize + test si email deja existant
+
+                $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+                $sameEmail = 0;
+                $msgEmail = "";
+                foreach($allUsers1 as $aUser){
+                    if($aUser->getEmail() == $email){
+                        $sameEmail = 1;
+                        $msgEmail = "<p>L'email est deja pris</p>";
+                    }
+                }
+
+                // sanitize + test si pseudo deja existant
+
+                $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $samePseudo = 0;
+                $msgPseudo = "";
+                foreach($allUsers2 as $aUser){
+                    if($aUser->getPseudo() == $pseudo){
+                        $samePseudo = 1;
+                        $msgPseudo = "<p>Le pseudo est deja pris</p>";
+                    }
+                }
+
+                // assénissement des mots de passe + comparraison
+
+                $password1 = filter_input(INPUT_POST, "password1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $password2 = filter_input(INPUT_POST, "password2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $notSamePassword = 0;
+                $msgPassword = "";
+                if($password1 != $password2){
+                    $notSamePassword = 1;
+                    $msgPassword = "<p>Les mots de passe sont differant</p>";
+                }
+                else{
+                    $hashedPassword = password_hash($password1, PASSWORD_DEFAULT);
+                }
+                $msg = $msgEmail.$msgPseudo.$msgPassword;
+                if($email && $sameEmail == 0 && $pseudo && $samePseudo == 0 && $hashedPassword && $notSamePassword == 0){
+                    $userManager->add($data = [
+                        "password" => $hashedPassword ,
+                        "pseudo" => $pseudo ,
+                        "email" => $email 
+                    ]);
+                    $this->redirectTo("security","login");
+                }
+
+            }
+        }
+
+        public function loginTry(){
+            if(isset($_POST['submit'])){
+                
+            }
+        }
     }
