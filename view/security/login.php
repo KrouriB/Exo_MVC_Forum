7ee014cfe7e1601359a81e6d0f9b6692ffc46d22
KrouriB <brice.krouri@gmail.com>
2023-04-03 16:16:15 +0200

    formulaire de connexion

--- view/security/login.php
+++ view/security/login.php
@@ -1,7 +1,17 @@
 <?php
 ?>
-<form action="index.php?ctrl=security&action=loginTry" method="post">
-    
+<form action="index.php?ctrl=security&action=loginTry" method="post" class="formPage">
+    <div class="uneInfoLog">
+        <label for="pseudo">Pseudo : </label>
+        <input type="text" name="pseudo" id="pseudo">
+    </div>
+    <div class="uneInfoLog">
+        <label for="password">Mot de Passe : </label>
+        <div>
+            <input type="text" name="password" id="password">
+            <button onclick="showPassword()"><i class="fa-regular fa-eye"></i></button>
+        </div>
+    </div>
     <div>
         <input type="submit" value="Connectez-Vous" name="submit">
     </div>
