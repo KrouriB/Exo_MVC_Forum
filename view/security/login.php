9085e6c0abcfdb7126c3f6e39a9050071941accd
KrouriB <brice.krouri@gmail.com>
2023-04-03 15:52:44 +0200

    creation fichier page de connexion

---
+++ view/security/login.php
@@ -0,0 +1,8 @@
+<?php
+?>
+<form action="index.php?ctrl=security&action=loginTry" method="post">
+    
+    <div>
+        <input type="submit" value="Connectez-Vous" name="submit">
+    </div>
+</form>
---
+++ view/security/register.php
@@ -0,0 +1,8 @@
+<?php
+?>
+<form action="index.php?ctrl=security&action=newRegister" method="post">
+
+    <div>
+        <input type="submit" value="Inscrivez-Vous" name="submit">
+    </div>
+</form>
