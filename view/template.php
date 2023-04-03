4ea5c9ec7c3fdda3acb31af752171daf6e14a086
KrouriB <brice.krouri@gmail.com>
2023-04-03 15:53:43 +0200

    modif direction lien

--- view/template.php
+++ view/template.php
@@ -39,8 +39,8 @@
                     }
                     else{
                         ?>
-                        <a href="./view/security/login.php">Connexion</a>
-                        <a href="./view/security/register.html">Inscription</a>
+                        <a href="index.php?ctrl=security&action=login">Connexion</a>
+                        <a href="index.php?ctrl=security&action=register">Inscription</a>
                         <a href="index.php?ctrl=forum&action=listTopics">la liste des topics</a>
                         <a href="index.php?ctrl=forum&action=listCategories">la liste des categories</a>
                     <?php
