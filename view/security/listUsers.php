fe839324227da8e86aecc44d1a82584fbeab3db1
KrouriB <brice.krouri@gmail.com>
2023-04-05 11:28:18 +0200

    affichage des users et info

--- view/security/listUsers.php
+++ view/security/listUsers.php
@@ -6,10 +6,28 @@ $users = $result["data"]['users'];
 
 <h1>liste users</h1>
 
-<?php
-foreach($users as $user){
-
-    ?>
-    <p><?=$user->getPseudo()?></p>
+<table>
+    <thead>
+        <tr>
+            <th>Pseudo</th>
+            <th>Email</th>
+            <th>Role</th>
+        </tr>
+    </thead>
+    <tbody>    
     <?php
-}
+    foreach($users as $user){
+        $roleFull = explode('_',$user->getRole());
+        $role = strtolower(end($roleFull));
+        
+        ?>
+        <tr>
+            <td><?=$user->getPseudo()?></td>
+            <td><?=$user->getEmail()?></td>
+            <td><?=$role?></td>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
