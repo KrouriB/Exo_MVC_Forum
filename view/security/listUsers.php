c7fa2dd0eb31130ca8012a5acdac498aecc1507c
KrouriB <brice.krouri@gmail.com>
2023-06-20 10:57:47 +0200

    div pour gerer affichage

--- view/security/listUsers.php
+++ view/security/listUsers.php
@@ -6,28 +6,32 @@ $users = $result["data"]['users'];
 
 <h1>liste users</h1>
 
-<table>
-    <thead>
-        <tr>
-            <th>Pseudo</th>
-            <th>Email</th>
-            <th>Role</th>
-        </tr>
-    </thead>
-    <tbody>    
-    <?php
-    foreach($users as $user){
-        $roleFull = explode('_',$user->getRole());
-        $role = strtolower(end($roleFull));
-        
-        ?>
-        <tr>
-            <td><?=$user->getPseudo()?></td>
-            <td><?=$user->getEmail()?></td>
-            <td><?=$role?></td>
-        </tr>
-        <?php
-    }
-    ?>
-    </tbody>
-</table>
+<div id="user-container">
+    <div id="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Pseudo</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                </tr>
+            </thead>
+            <tbody>    
+                <?php
+            foreach($users as $user){
+                $roleFull = explode('_',$user->getRole());
+                $role = strtolower(end($roleFull));
+                
+                ?>
+                <tr>
+                    <td><?=$user->getPseudo()?></td>
+                    <td><?=$user->getEmail()?></td>
+                    <td><?=$role?></td>
+                </tr>
+                <?php
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
+</div>
