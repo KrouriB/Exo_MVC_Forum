ca99d94c5992538a9a245bae9e4e5bb62aabc964
KrouriB <brice.krouri@gmail.com>
2023-06-20 16:01:14 +0200

    modif affichage si connecter

--- view/home.php
+++ view/home.php
@@ -2,31 +2,21 @@
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
-<p>
-    <a href="index.php?ctrl=security&action=login">Se connecter</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="index.php?ctrl=security&action=register">S'inscrire</a>
-</p>
-<!--
-<div class="bloc connect">
-    <h1>Connectez Vous</h1>
-    
-    <form>
-        <div>
-            <label for="">Pseudo :</label>
-            <input type="text" name="" id="">
-        </div>
-        <div>
-            <label for="">Mot de Passe :</label>
-            <input type="text" name="" id="">
-        </div>
-        <div>
-            <input type="submit" value="Accpter">
-        </div>
-    </form>
-    <div>
-        <p>Vous n'etes pas encore inscrit ?</p>
-        <a href="/security/register.html">Inscrivez vous ici</a>
-    </div>
+<div id="message-acceuil">
+    <?php
+        if(App\Session::getUser() == false){
+            ?>
+                <p>
+                    <a href="index.php?ctrl=security&action=login">Se connecter</a>
+                    <span>&nbsp;-&nbsp;</span>
+                    <a href="index.php?ctrl=security&action=register">S'inscrire</a>
+                </p>
+            <?php
+        }
+        else{
+            ?>
+                <p>Vous êtes connecté !</p>
+            <?php
+        }
+    ?>
 </div>
--->
