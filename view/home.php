c55b4eb768ec6f8041000ba4a6115d1a19e1d2b6
KrouriB <brice.krouri@gmail.com>
2023-06-20 16:21:20 +0200

    ajout div pour affichage

--- view/home.php
+++ view/home.php
@@ -1,22 +1,25 @@
-<h1>BIENVENUE CHEZ LES PTIT'S ELANS</h1>
+<div id="home-container">
+    <div id="entete">
+        <h1>BIENVENUE CHEZ LES PTIT'S ELANS</h1>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
-
-<div id="message-acceuil">
-    <?php
-        if(App\Session::getUser() == false){
-            ?>
-                <p>
-                    <a href="index.php?ctrl=security&action=login">Se connecter</a>
-                    <span>&nbsp;-&nbsp;</span>
-                    <a href="index.php?ctrl=security&action=register">S'inscrire</a>
-                </p>
-            <?php
-        }
-        else{
-            ?>
-                <p>Vous êtes connecté !</p>
-            <?php
-        }
-    ?>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+    </div>
+    <div id="message-acceuil">
+        <?php
+            if(App\Session::getUser() == false){
+                ?>
+                    <p>
+                        <a href="index.php?ctrl=security&action=login">Se connecter</a>
+                        <span>&nbsp;-&nbsp;</span>
+                        <a href="index.php?ctrl=security&action=register">S'inscrire</a>
+                    </p>
+                <?php
+            }
+            else{
+                ?>
+                    <p>Vous êtes connecté !</p>
+                <?php
+            }
+        ?>
+    </div>
 </div>
