bf570063f083b8d36b60d928e99d843aec4fdb87
KrouriB <brice.krouri@gmail.com>
2023-06-21 22:26:48 +0200

    ajout div pour agencement

--- view/security/login.php
+++ view/security/login.php
@@ -1,18 +1,18 @@
-<?php
-?>
-<form action="index.php?ctrl=security&action=loginTry" method="post" class="formPage">
-    <div class="uneInfoLog">
-        <label for="email">Email : </label>
-        <input type="email" name="email" id="email">
-    </div>
-    <div class="uneInfoLog">
-        <label for="password">Mot de Passe : </label>
-        <div>
-            <input type="password" name="password" id="password" class="mdp">
-            <input type="checkbox" onclick="showPasswordlog()"><i class="fa-regular fa-eye"></i>
+<div id="login-container">
+    <form action="index.php?ctrl=security&action=loginTry" method="post" class="formPage">
+        <div class="uneInfoLog">
+            <label for="email">Email : </label>
+            <input type="email" name="email" id="email">
         </div>
-    </div>
-    <div>
-        <input type="submit" value="Connectez-Vous" name="submit">
-    </div>
-</form>
+        <div class="uneInfoLog">
+            <label for="password">Mot de Passe : </label>
+            <div>
+                <input type="password" name="password" id="password" class="mdp">
+                <input type="checkbox" onclick="showPasswordlog()"><i class="fa-regular fa-eye"></i>
+            </div>
+        </div>
+        <div>
+            <input type="submit" value="Connectez-Vous" name="submit">
+        </div>
+    </form>
+</div>
--- view/security/register.php
+++ view/security/register.php
@@ -1,26 +1,26 @@
-<?php
-?>
-<form action="index.php?ctrl=security&action=newRegister" method="post" class="formPage">
-    <div class="uneInfoLog">
-        <label for="email">Email : </label>
-        <input type="email" name="email" id="email">
-    </div>
-    <div class="uneInfoLog">
-        <label for="pseudo">Pseudo : </label>
-        <input type="text" name="pseudo" id="pseudo">
-    </div>
-    <div class="uneInfoLog">
-        <label for="password1">Mot de Passe : </label>
-        <div>
-            <input type="password" name="password1" id="password1" class="mdp">
-            <input type="checkbox" onclick="showPasswordreg()"><i class="fa-regular fa-eye"></i>
+<div id="register-container">
+    <form action="index.php?ctrl=security&action=newRegister" method="post" class="formPage">
+        <div class="uneInfoLog">
+            <label for="email">Email : </label>
+            <input type="email" name="email" id="email">
         </div>
-    </div>
-    <div class="uneInfoLog">
-        <label for="password2">Veuillez Conffirmez le Mot de Passe : </label>
-        <input type="password" name="password2" id="password2">
-    </div>
-    <div>
-        <input type="submit" value="Inscrivez-Vous" name="submit">
-    </div>
-</form>
+        <div class="uneInfoLog">
+            <label for="pseudo">Pseudo : </label>
+            <input type="text" name="pseudo" id="pseudo">
+        </div>
+        <div class="uneInfoLog">
+            <label for="password1">Mot de Passe : </label>
+            <div>
+                <input type="password" name="password1" id="password1" class="mdp">
+                <input type="checkbox" onclick="showPasswordreg()"><i class="fa-regular fa-eye"></i>
+            </div>
+        </div>
+        <div class="uneInfoLog">
+            <label for="password2">Veuillez Conffirmez le Mot de Passe : </label>
+            <input type="password" name="password2" id="password2">
+        </div>
+        <div>
+            <input type="submit" value="Inscrivez-Vous" name="submit">
+        </div>
+    </form>
+</div>
