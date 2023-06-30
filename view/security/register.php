ad54c4d5e9619ddff46413d164995b6d9401a60c
KrouriB <brice.krouri@gmail.com>
2023-06-30 17:29:51 +0200

    modified for css

--- view/security/register.php
+++ view/security/register.php
@@ -4,28 +4,48 @@
 ?>
 
 <div id="container">
-    <form action="index.php?ctrl=security&action=newRegister" method="post" class="formPage">
-        <div class="uneInfoLog">
-            <label for="email">Email : </label>
-            <input type="email" name="email" id="email">
-        </div>
-        <div class="uneInfoLog">
-            <label for="pseudo">Pseudo : </label>
-            <input type="text" name="pseudo" id="pseudo">
-        </div>
-        <div class="uneInfoLog">
-            <label for="password1">Mot de Passe : </label>
-            <div>
-                <input type="password" name="password1" id="password1" class="mdp">
-                <input type="checkbox" onclick="showPasswordreg()"><i class="fa-regular fa-eye"></i>
+    <form action="index.php?ctrl=security&action=newRegister" method="post" id="formPage">
+        <div id=blocCompte>
+            <div class="uneColForm">
+                <div class="uneInfoReg">
+                    <label for="email">Email : </label>
+                    <div class="blocARemplir">
+                        <span><i class="fa-solid fa-at"></i></span>
+                        <input type="email" name="email" id="email">
+                    </div>
+                </div>
+                <div class="uneInfoReg">
+                    <label for="password1">Mot de Passe : </label>
+                    <div class="blocARemplir">
+                        <span><i class="fa-solid fa-key"></i></span>
+                        <input type="password" name="password1" id="password1">
+                    </div>
+                </div>
+            </div>
+            <div class="uneColForm">
+                <div class="uneInfoReg">
+                    <label for="pseudo">Pseudo : </label>
+                    <div class="blocARemplir">
+                        <span><i class="fa-solid fa-user"></i></span>
+                        <input type="text" name="pseudo" id="pseudo">
+                    </div>
+                </div>
+                <div class="uneInfoReg">
+                    <label for="password2">Veuillez Conffirmez le Mot de Passe : </label>
+                    <div class="blocARemplir">
+                        <span><i class="fa-solid fa-key"></i></span>
+                        <input type="password" name="password2" id="password2">
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="uneInfoLog">
-            <label for="password2">Veuillez Conffirmez le Mot de Passe : </label>
-            <input type="password" name="password2" id="password2">
-        </div>
-        <div>
-            <input type="submit" value="Inscrivez-Vous" name="submit">
+        <div id="blocButton">
+            <input type="reset" value="Videz le Formulaire" class="notCheckInput">
+            <input type="submit" value="Inscrivez-Vous" name="submit" class="notCheckInput">
+            <div>
+                <input type="checkbox" onclick="showPasswordreg()">
+                <span><i class="fa-regular fa-eye"></i></span>
+            </div>
         </div>
     </form>
 </div>
