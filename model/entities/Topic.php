c0a4839f1b7ab271a7a3fe9a38ab40cb11f7da2d
KrouriB <brice.krouri@gmail.com>
2023-07-05 16:06:06 +0200

    add Get/Set Total

--- model/entities/Topic.php
+++ model/entities/Topic.php
@@ -209,4 +209,24 @@
 
                         return $this;
                 }
+
+                /**
+                 * Get the value of total
+                 */ 
+                public function getTotal()
+                {
+                        return $this->total;
+                }
+
+                /**
+                 * Set the value of total
+                 *
+                 * @return  self
+                 */ 
+                public function setTotal($total)
+                {
+                        $this->total = $total;
+
+                        return $this;
+                }
         }
--- model/entities/User.php
+++ model/entities/User.php
@@ -115,6 +115,26 @@
                         return $this;
                 }
 
+                /**
+                 * Get the value of total
+                 */ 
+                public function getTotal()
+                {
+                        return $this->total;
+                }
+
+                /**
+                 * Set the value of total
+                 *
+                 * @return  self
+                 */ 
+                public function setTotal($total)
+                {
+                        $this->total = $total;
+
+                        return $this;
+                }
+
                 public function hasRole($role): bool
                 {
                         $result = $this->role == $role;
